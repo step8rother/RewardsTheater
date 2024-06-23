0b003d4f5a8a1624ec0df7bcd0c534b1b2cc20a3
Lev Leontev <lev.leontev@jetbrains.com>
2024-06-23 13:11:06 +0200

    boost fpic

--- .github/scripts/linux-deps.sh
+++ .github/scripts/linux-deps.sh
@@ -12,8 +12,8 @@ if [[ ! -d ./boost ]]; then
   wget -O boost.tar.gz https://archives.boost.io/release/1.85.0/source/boost_1_85_0.tar.gz
   tar -xzf boost.tar.gz --strip-components=1
   rm boost.tar.gz
-  ./bootstrap.sh link=static
-  ./b2
+  ./bootstrap.sh link=static --with-libraries=system,url,json
+  ./b2 cxxflags="-fPIC" cflags="-fPIC"
   popd
 else
   echo "Boost directory exists, skipping build"
